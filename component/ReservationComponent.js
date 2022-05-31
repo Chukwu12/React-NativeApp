2e27c02e0228d7dd4177f4fe956842ebadddc7be
Okechukwu Egeruoh <oegeruoh@gmail.com>
2022-05-31 15:20:33 -0400

    Local Notifications

--- component/ReservationComponent.js
+++ component/ReservationComponent.js
@@ -4,6 +4,7 @@ import { Text, View, ScrollView, StyleSheet,
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
 import { campsitesFailed } from '../redux/ActionCreators';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -40,6 +41,32 @@ class Reservation extends Component {
             showCalendar: false,
             showModal: false
         });
+    }
+
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
     }
                
     render() {
@@ -111,7 +138,11 @@ class Reservation extends Component {
                                  
                              },
                              {
-                                 text:'OK'
+                                text: 'OK', 
+                                onPress: () => {
+                                    this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                                    this.resetForm();
+                                }
                              }
                             ]
                         )}
